e124d582bc0ca2f52f2b258b196245309881fcb0
Evgenij <evgshul@gmail.com>
2018-11-13 23:11:35 +0200

    DateTime Parsing datetimeFormaterBuilder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,63 +1,29 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = new int[10];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(reader.readLine());
-        }
-
-        sort(array);
-
-        System.out.println(array[0]);
-        System.out.println(array[1]);
-        System.out.println(array[2]);
-        System.out.println(array[3]);
-        System.out.println(array[4]);
+        System.out.println(isDateOdd("MAY 1 2013"));
     }
 
-    public static void sort(int[] array) {
-        //напишите тут ваш код
+    public static boolean isDateOdd(String date) {
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive()//metod ignora registra
+                .appendPattern("MMMM d yyyy")//vtnjl ghbvtytybz gfnthyf
+                .toFormatter();//
+        LocalDate date1 = LocalDate.parse(date, formatter);
 
-        for (int j = array.length-1; j > 0; j--) {
-            for (int i = 1; i < array.length; i++) {
-                int tmp;
-                if (array[i] > array[i - 1]) {
-                    tmp = array[i - 1];
-                    array[i - 1] = array[i];
-                    array[i] = tmp;
-                }
-            }
-        }
-
-
-       /* Integer[]rev = new Integer[array.length];
-        for(int i =0; i<array.length;i++){
-            rev[i]=array[i];
-        }
-        Arrays.sort(rev,Collections.reverseOrder());
-        for(int i=0;i<array.length;i++){
-            array[i]=rev[i];
-        }*/
-
-
+        if (date1.getDayOfYear() % 2 != 0) return true;
+        else return false;
     }
 }
-
-
-
-
-
-
-
-
 
 
 
