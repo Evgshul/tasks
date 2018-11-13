ee809a935945bd084f18ecfd262dc29fcfc87fe2
Evgenij <evgshul@gmail.com>
2018-11-13 11:38:55 +0200

    Task: reverse array sorting bubble method and whoth INTEGER Array, sort() and reverse()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,18 +9,45 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = reader.readLine();
-
-        String[] w = s.split(" +");// razdeljaem stroku na slova, uchitivaem chto mozhet bit probelov >1
-        for (int i = 0; i < w.length; i++) {
-            if (w[i].length() != 0) {//check is first string is " "
-                String a = w[i].substring(0, 1).toUpperCase() + w[i].substring(1);
-                //menjaem 1 bukvu na zaglavnuju
-
-                System.out.print(a + " ");
-            }
-
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
         }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+
+        for (int j = array.length-1; j > 0; j--) {
+            for (int i = 1; i < array.length; i++) {
+                int tmp;
+                if (array[i] > array[i - 1]) {
+                    tmp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = tmp;
+                }
+            }
+        }
+
+
+       /* Integer[]rev = new Integer[array.length];
+        for(int i =0; i<array.length;i++){
+            rev[i]=array[i];
+        }
+        Arrays.sort(rev,Collections.reverseOrder());
+        for(int i=0;i<array.length;i++){
+            array[i]=rev[i];
+        }*/
+
+
     }
 }
 
