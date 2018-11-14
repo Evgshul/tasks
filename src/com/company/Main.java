4a2b1ee560f2bbde88e93696609c57e4d2495c23
Evgenij <evgshul@gmail.com>
2018-11-14 15:11:01 +0200

    String sort (bubble sort)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,51 @@
 package com.company;
 
-import java.time.LocalDate;
-
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println(isDateOdd("MAY 1 2013"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
     }
 
-    public static boolean isDateOdd(String date) {
-        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-                .parseCaseInsensitive()//metod ignora registra
-                .appendPattern("MMMM d yyyy")//vtnjl ghbvtytybz gfnthyf
-                .toFormatter();//
-        LocalDate date1 = LocalDate.parse(date, formatter);
+    public static void sort(String[] array) {
+        //напишите тут ваш код
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (isGreaterThan(array[i], array[j])) {
+                    String t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                }
+            }
+        }
 
-        if (date1.getDayOfYear() % 2 != 0) return true;
-        else return false;
+
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
     }
 }
+
+
+
+
+
 
 
 
