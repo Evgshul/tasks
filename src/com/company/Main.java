b47e7ebac0d5b75784e0297b242bb311e60abbb7
Evgenij <evgshul@gmail.com>
2018-11-21 22:32:52 +0200

    Java string tokens

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,31 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] array = new String[5];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = reader.readLine();
-        }
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        s.trim();
+        String[] a = s.split("[ ._,?!']+");
 
-        sort(array);
+        if (s == ""||s.equals("")||s.trim().equals("")) System.out.println(0);
+        if (a.length > 400000) return;
+        else if (!s.trim().equals("")) System.out.println(a.length);
 
-        for (String x : array) {
-            System.out.println(x);
-        }
-    }
-
-    public static void sort(String[] array) {
-        //напишите тут ваш код
-        for (int i = 0; i <array.length ; i++) {
-            for (int j = i+1; j < array.length; j++) {
-                if (isGreaterThan(array[i], array[j])) {
-                    String t = array[i];
-                    array[i] = array[j];
-                    array[j] = t;
-                }
-            }
-        }
-
+        for (String r : a) System.out.println(r);
+        scan.close();
 
     }
 
-    //Метод для сравнения строк: 'а' больше чем 'b'
-    public static boolean isGreaterThan(String a, String b) {
-        return a.compareTo(b) > 0;
-    }
 }
+
+
+
+
 
 
 
