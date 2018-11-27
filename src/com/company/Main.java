0f7218d7e1c7693a730639c0b754c512f45dc817
Evgenij <evgshul@gmail.com>
2018-11-27 10:29:54 +0200

    Change date format from yyyy-MM-dd to MMM dd, yyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,32 @@
 package com.company;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        s.trim();
-        String[] a = s.split("[ ._,?!']+");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = dateFormat.parse(reader.readLine());
+        SimpleDateFormat dateFormat1 = new SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH);
+        System.out.println(dateFormat1.format(date).toUpperCase());
 
-        if (s == ""||s.equals("")||s.trim().equals("")) System.out.println(0);
-        if (a.length > 400000) return;
-        else if (!s.trim().equals("")) System.out.println(a.length);
 
-        for (String r : a) System.out.println(r);
-        scan.close();
 
-    }
+        }
 
 }
+
+
+
+
+
 
 
 
