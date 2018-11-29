bf313b1a586b0b737948f201564a6e2333d40d7b
Evgenij <evgshul@gmail.com>
2018-11-29 14:04:17 +0200

    Sorting task !Like! input String and number sorting strings in increasing order Sorting numbers in decreasing order

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,24 +2,77 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
 
 public class Main {
 
-
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = dateFormat.parse(reader.readLine());
-        SimpleDateFormat dateFormat1 = new SimpleDateFormat("MMM dd, yyyy", Locale.ENGLISH);
-        System.out.println(dateFormat1.format(date).toUpperCase());
-
-
-
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String s = reader.readLine();
+            if (s.isEmpty()) break;
+            list.add(s);
         }
+
+        String[] array = list.toArray(new String[list.size()]);
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        // напишите тут ваш код
+
+        String tmp = "";
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (isNumber(array[i]) == true && isNumber(array[j]) == true) {
+                    while (Integer.parseInt(array[i]) < Integer.parseInt(array[j])) {
+                        tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j=i+1;j<array.length;j++){
+                if (isNumber(array[i]) == false&&isNumber(array[j])==false) {
+                    if (isGreaterThan(array[i], array[j])==true) {
+                        tmp = array[j];
+                        array[j] = array[i];
+                        array[i] = tmp;
+                    }}
+            }
+        }
+    }
+
+
+    // Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+
+
+    // Переданная строка - это число?
+    public static boolean isNumber(String s) {
+        if (s.length() == 0) return false;
+
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if ((i != 0 && c == '-') // Строка содержит '-'
+                    || (!Character.isDigit(c) && c != '-') // или не цифра и не начинается с '-'
+                    || (chars.length == 1 && c == '-')) // или одиночный '-'
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
 
